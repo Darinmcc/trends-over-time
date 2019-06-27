7f08144e300bd6d56e0728a2ed14184de81dfd76
Darinmcc <50928612+Darinmcc@users.noreply.github.com>
2019-06-26 21:01:01 -0400

    project change

--- app/trends_over_time.py
+++ app/trends_over_time.py
@@ -1,21 +1,32 @@
-"trends over time"
+"News"
+
+import requests # to make requests for https package - need to install request package in virtual environme
+from dotenv import load_dotenv
+
+import json #use to convert json string to dictionary #module don't need to install in virtual part of python
+import datetime #module
+import calendar # module
+import csv # module
+import os # module
+import statistics # statistic module
+import pandas
+
+load_dotenv() #> loads contents of the .env file into the script's environment
 
 
-
-
-print("-------------------------")
-print("SELECTED GOOGLE SEARCH: ")
-print("-------------------------")
-print("REQUESTING GOOGLE SEARCH TRENDS...")
-print("REQUEST AT: 2018-02-20 02:00pm")
-print("-------------------------")
-print("LATEST DAY: 2018-02-20")
-print("LATEST CLOSE: $100,000.00")
-print("RECENT HIGH: $101,000.00")
-print("RECENT LOW: $99,000.00")
-print("-------------------------")
-print("RECOMMENDATION: BUY!")
-print("RECOMMENDATION REASON: TODO")
-print("-------------------------")
-print("HAPPY INVESTING!")
-print("-------------------------")
+#print("-------------------------")
+#print("SELECTED GOOGLE SEARCH: ")
+#print("-------------------------")
+#print("REQUESTING GOOGLE SEARCH TRENDS...")
+#print("REQUEST AT: 2018-02-20 02:00pm")
+#print("-------------------------")
+#print("LATEST DAY: 2018-02-20")
+#print("LATEST CLOSE: $100,000.00")
+#print("RECENT HIGH: $101,000.00")
+#print("RECENT LOW: $99,000.00")
+#print("-------------------------")
+#print("RECOMMENDATION: BUY!")
+#print("RECOMMENDATION REASON: TODO")
+#print("-------------------------")
+#print("HAPPY INVESTING!")
+#print("-------------------------")
